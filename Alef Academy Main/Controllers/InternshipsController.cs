15afc2d9d8784401565cb85bae17a795f554fe8c
TafadzwaTapera <w65242@student.wsizedu.pl>
2024-08-11 19:55:22 +0200

    latest update

--- Alef Academy Main/Controllers/InternshipsController.cs
+++ Alef Academy Main/Controllers/InternshipsController.cs
@@ -28,24 +28,6 @@ namespace Alef_Academy_Main.Controllers
             return View(await _context.Internships.ToListAsync());
         }
 
-        // GET: Internships/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var internships = await _context.Internships
-                .FirstOrDefaultAsync(m => m.applicationid == id);
-            if (internships == null)
-            {
-                return NotFound();
-            }
-
-            return View(internships);
-        }
-
         // GET: Internships/Create
         public IActionResult Create()
         {
@@ -77,7 +59,7 @@ namespace Alef_Academy_Main.Controllers
                     _context.Internships.Add(internship);
                     _context.SaveChanges();
 
-                    return RedirectToAction(nameof(Index));
+                    return RedirectToAction("ApplicationSucceess,Internship");
                 }
                 catch (Exception ex)
                 {
@@ -88,90 +70,11 @@ namespace Alef_Academy_Main.Controllers
             return View(internship);
         }
 
-
-        // GET: Internships/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public IActionResult ApplicationSuccess()
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var internships = await _context.Internships.FindAsync(id);
-            if (internships == null)
-            {
-                return NotFound();
-            }
-            return View(internships);
+            return View();
         }
-
-        // POST: Internships/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("applicationid,position,name,email,cvfilename,cvfiledata,portfoliolink,isawareofunpaid,isavailableasap,iscommittothreemonths,isawareofcommitment,applicationdate")] Internships internships)
-        {
-            if (id != internships.applicationid)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(internships);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!InternshipsExists(internships.applicationid))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(internships);
-        }
-
-        // GET: Internships/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var internships = await _context.Internships
-                .FirstOrDefaultAsync(m => m.applicationid == id);
-            if (internships == null)
-            {
-                return NotFound();
-            }
-
-            return View(internships);
-        }
-
-        // POST: Internships/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var internships = await _context.Internships.FindAsync(id);
-            if (internships != null)
-            {
-                _context.Internships.Remove(internships);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        
 
         private bool InternshipsExists(int id)
         {
